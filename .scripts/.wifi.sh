5d595329082b2a1893f55cf93a05f411667bc49b
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2021-09-08 21:48:22 +0200

    Deleted wifi drivers and script - to move to another repository

--- .scripts/.wifi.sh
+++
@@ -1,9 +0,0 @@
-cd ~/.essentials/
-tar xfv RTL88x2BU-Linux-Driver-master.tar.gz
-cd RTL88x2BU-Linux-Driver-master
-make
-sudo make install
-modprobe 882xbu
-cd ..
-rm -rv RTL88x2BU-Linux-Driver-master
-cd ~
